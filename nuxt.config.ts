e9f95530ffe9bc2515fe84e1b3de093d513c25aa
caiooliveirah09 <leelros@hotmail.com>
2023-02-23 23:38:02 -0300

    changed the vuetify configuration in nuxt, and added NuxtPages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: [
-    "vuetify/lib/styles/main.sass",
-    "@mdi/font/css/materialdesignicons.min.css",
-  ],
+  css: ["vuetify/styles/main.sass", "@mdi/font/css/materialdesignicons.css"],
   build: {
     transpile: ["vuetify"],
   },
