d8936f2ac0f1393c3358475f279a3c30d87878f0
caiooliveirah09 <leelros@hotmail.com>
2023-02-27 11:23:33 -0300

    created login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,33 +1,34 @@
 <template>
   <v-app>
-    <v-main>
-      <v-card>
-        <h1>Log in</h1>
-        <v-form @submit.prevent="login">
-          <label class="label" for="email">Email:</label>
-          <v-text-field id="email" v-model="email" type="email" name="email" />
-          <div v-if="emailError">{{ emailError }}</div>
-          <br /><br />
-          <label class="label" for="password">Password:</label>
-          <v-text-field
-            id="password"
-            v-model="password"
-            type="password"
-            name="password"
-          />
-          <div v-if="passwordError">{{ passwordError }}</div>
-          <br /><br />
-          <v-btn type="submit" :disabled="isDisabled">log in</v-btn>
-          <v-btn flat to="/register" nuxt>create new account</v-btn>
-          <div v-if="loginError">{{ loginError }}</div>
-        </v-form>
-      </v-card>
-    </v-main>
+    <v-card>
+      <h1>Log in</h1>
+      <v-form @submit.prevent="login">
+        <label class="label" for="email">Email:</label>
+        <v-text-field id="email" v-model="email" type="email" name="email" />
+        <div v-if="emailError">{{ emailError }}</div>
+        <label class="label" for="password">Password:</label>
+        <v-text-field
+          id="password"
+          v-model="password"
+          type="password"
+          name="password"
+        />
+        <div v-if="passwordError">{{ passwordError }}</div>
+        <v-btn type="submit" :disabled="isDisabled">log in</v-btn>
+        <v-btn flat to="/register" nuxt>create new account</v-btn>
+        <div v-if="loginError">{{ loginError }}</div>
+      </v-form>
+    </v-card>
   </v-app>
 </template>
 
 <script>
 export default {
+  name: "index",
+  mounted() {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) navigateTo("/");
+  },
   data() {
     return {
       email: "",
@@ -44,7 +45,6 @@ export default {
   },
   methods: {
     validateEmail() {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       if (!this.email) {
         this.emailError = "Please, type your e-mail";
       } else {
@@ -74,12 +74,20 @@ export default {
     },
 
     login() {
-      const users = JSON.parse(localStorage.getItem("user"));
-      const user = users.find(
-        (user) => user.email === this.email && user.password === this.password
-      );
-      if (user) navigateTo("/");
-      else this.loginError = "Wrong email or password";
+      const users = JSON.parse(localStorage.getItem("users"));
+      if (users) {
+        const user = users.find(
+          (user) => user.email === this.email && user.password === this.password
+        );
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user));
+          navigateTo("/");
+        } else {
+          this.loginError = "Wrong email or password";
+        }
+      } else {
+        navigateTo("/register");
+      }
     },
   },
 };
